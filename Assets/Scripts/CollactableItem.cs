48dedbd0f230945fdd54ecbbc3db5849abf0c4c4
brunocalou <brunocaloualves@gmail.com>
2016-07-20 21:38:50 -0300

    Added collatable item and inventory communication. Now, when a collatable item is clicked, the item goes to the correct position of the inventory (on screen)

--- Assets/Scripts/CollactableItem.cs
+++ Assets/Scripts/CollactableItem.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class CollactableItem : MonoBehaviour {
 
+    public Inventory inventory;
 	// Use this for initialization
 	void Start () {
-	
+        inventory = GameObject.FindWithTag("Inventory").GetComponent<Inventory>();
 	}
 
     // Update is called once per frame
@@ -15,12 +16,19 @@ public class CollactableItem : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.R))
         {
             this.gameObject.GetComponent<Renderer>().enabled = true; //Show the object
+            this.gameObject.GetComponent<BoxCollider>().enabled = true;
         }
     }
 
     void OnMouseDown()
     {
         Debug.Log("MOUSE DOWN");
+        if (inventory != null)
+        {
+            Debug.Log("Item will be added");
+            inventory.addItem(this);
+        }
         this.gameObject.GetComponent<Renderer>().enabled = false; //Hide the object
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;//Disable the body so the user can't click it
     }
 }
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+    ArrayList items;
+    const int MAX_NUMBER_OF_ITEMS = 5;
+
+	// Use this for initialization
+	void Start () {
+        items = new ArrayList(MAX_NUMBER_OF_ITEMS);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void addItem(CollactableItem item)
+    {
+        Debug.Log("Added item");
+        items.Add(item);
+
+        //Place item on the inventory
+        GameObject new_item = Instantiate(item.gameObject);
+        UnityEngine.Canvas canvas = FindObjectOfType<Canvas>();
+        Destroy(new_item.GetComponent<CollactableItem>());
+
+        new_item.transform.SetParent(canvas.transform, false);
+        new_item.transform.localPosition = new Vector3(512, 200 - (items.Count - 1) * 100, -10);
+        new_item.transform.SetAsFirstSibling();
+        
+        
+        Debug.Log(item);
+    }
+}
