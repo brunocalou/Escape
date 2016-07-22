f00b39769d5aee29af6d805ed4cdb34a336af943
brunocalou <brunocaloualves@gmail.com>
2016-07-22 10:31:14 -0300

    Fixed inventory size

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -26,10 +26,20 @@ public class Inventory : MonoBehaviour {
         Destroy(new_item.GetComponent<CollactableItem>());
 
         new_item.transform.SetParent(canvas.transform, false);
-        new_item.transform.localPosition = new Vector3(512, 200 - (items.Count - 1) * 100, -10);
         new_item.transform.SetAsFirstSibling();
-        
-        
+
+        Destroy(new_item.GetComponent<Transform>());
+
+        new_item.AddComponent<RectTransform>();
+        RectTransform inventory_rect_transform = this.GetComponent<RectTransform>();
+        RectTransform new_item_rect_transform = new_item.GetComponent<RectTransform>();
+        //new_item.transform.localPosition = new Vector3(this.transform.position.x, this.transform.position.y, -10);
+        new_item_rect_transform.anchoredPosition = new Vector2(1, 1);
+        new_item_rect_transform.anchorMin = new Vector2(1, 1);
+        new_item_rect_transform.anchorMax = new Vector2(1, 1);
+
+
+
         Debug.Log(item);
     }
 }
